09c9e883d21f60523efeb73e92cb849d5b33e658
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2020-10-21 12:31:10 +0300

    Create DbContext

--- Apps/SharedTrip/Data/ApplicationDbContext.cs
+++ Apps/SharedTrip/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿namespace SharedTrip.Data
+﻿using SharedTrip.Models;
+
+namespace SharedTrip.Data
 {
     using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +13,27 @@
         public ApplicationDbContext(DbContextOptions db)
             : base(db)
         {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Trip> Trips { get; set; }
+
+        public DbSet<UserTrip> UserTrips { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DELL-IT\SQLEXPRESS;Database=SharedTrip;Trusted_Connection=True;Integrated Security=True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserTrip>()
+                .HasKey(k => new {k.UserId, k.TripId});
+
+            //base.OnModelCreating(modelBuilder);
         }
     }
 }
